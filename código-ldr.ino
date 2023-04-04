2338679d9253ff0683eb4794721ee706fc6ca26f
pbrnx <128100284+pbrnx@users.noreply.github.com>
2023-04-04 15:21:28 -0300

    Create código-ldr.ino

---
+++ código-ldr.ino
@@ -0,0 +1,41 @@
+const int LDR_PIN = A0;
+const int GREEN_LED_PIN = 2;
+const int YELLOW_LED_PIN = 3;
+const int RED_LED_PIN = 4;
+const int BUZZER_PIN = 8;
+const int ALERT_THRESHOLD = 50; //isso é quanto o sensor capta de luminosidade
+
+void setup() {
+  pinMode(GREEN_LED_PIN, OUTPUT);
+  pinMode(YELLOW_LED_PIN, OUTPUT);
+  pinMode(RED_LED_PIN, OUTPUT);
+  pinMode(BUZZER_PIN, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  int lightValue = analogRead(LDR_PIN);
+  Serial.print("Luminosidade: "); //report no monitor serial
+  Serial.println(lightValue);
+  
+  if (lightValue == 969) { //969 é o equivalente a máxima luminosidade no tinkercad, na vida real varia e precisamos ajustar o valor conforme necessário. A leitura deve ser feita pelo 
+    digitalWrite(GREEN_LED_PIN, LOW);
+    digitalWrite(YELLOW_LED_PIN, LOW);
+    digitalWrite(RED_LED_PIN, HIGH);
+    tone(BUZZER_PIN, 2000);
+  }
+  else if (lightValue > ALERT_THRESHOLD) { //alert_threshold indica o valor do intervalo de luminosidade, nesse caso se for > que 50, a luz amarela se acende
+    digitalWrite(GREEN_LED_PIN, LOW);
+    digitalWrite(YELLOW_LED_PIN, HIGH);
+    digitalWrite(RED_LED_PIN, LOW);
+    tone(BUZZER_PIN, 1000);
+  } 
+  else { 
+    digitalWrite(GREEN_LED_PIN, HIGH); //se qualquer coisa fora das condições acima ocorrer, o verde se acende, ou seja, o verde só acende se o valor ideal de luminosidade estiver definido.
+    digitalWrite(YELLOW_LED_PIN, LOW);
+    digitalWrite(RED_LED_PIN, LOW);
+    noTone(BUZZER_PIN);
+  }
+  
+  delay(100); //quanto tempo entre cada leitura do monitor serial em ms
+}
