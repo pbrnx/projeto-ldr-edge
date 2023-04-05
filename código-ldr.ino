ddc7bcb1dd5c2fb5ccfd84d5b1cafe092c634f9f
pbrnx <128100284+pbrnx@users.noreply.github.com>
2023-04-05 19:38:03 -0300

    Update código-ldr.ino

--- código-ldr.ino
+++ código-ldr.ino
@@ -3,7 +3,7 @@ const int GREEN_LED_PIN = 2;
 const int YELLOW_LED_PIN = 3;
 const int RED_LED_PIN = 4;
 const int BUZZER_PIN = 8;
-const int ALERT_THRESHOLD = 50; //isso é quanto o sensor capta de luminosidade
+const int ALERT_THRESHOLD = 200; //isso é quanto o sensor capta de luminosidade
 
 unsigned long previousTime = 0;
 int buzzerState = LOW;
@@ -21,23 +21,17 @@ void loop() {
   Serial.print("Luminosidade: "); //report no monitor serial
   Serial.println(lightValue);
 
-  // checa se o ultimo beep foi há 3 segundos
-  if (millis() - previousTime >= 3000) {
-    previousTime = millis();
-    buzzerState = !buzzerState; 
-  }
-
-  if (lightValue == 969) { //969 é o equivalente a máxima luminosidade no tinkercad, na vida real varia e precisamos ajustar o valor conforme necessário. A leitura deve ser feita pelo 
+  if (lightValue >= 400  ) { //969 é o equivalente a máxima luminosidade no tinkercad, na vida real varia e precisamos ajustar o valor conforme necessário. A leitura deve ser feita pelo 
     digitalWrite(GREEN_LED_PIN, LOW);
     digitalWrite(YELLOW_LED_PIN, LOW);
     digitalWrite(RED_LED_PIN, HIGH);
-    tone(BUZZER_PIN, 2000 * buzzerState); 
+    tone(BUZZER_PIN, 2000);
   }
   else if (lightValue > ALERT_THRESHOLD) { //alert_threshold indica o valor do intervalo de luminosidade, nesse caso se for > que 50, a luz amarela se acende
     digitalWrite(GREEN_LED_PIN, LOW);
     digitalWrite(YELLOW_LED_PIN, HIGH);
     digitalWrite(RED_LED_PIN, LOW);
-    tone(BUZZER_PIN, 1000 * buzzerState); 
+    tone(BUZZER_PIN, 1000); 
   } 
   else { 
     digitalWrite(GREEN_LED_PIN, HIGH); //se qualquer coisa fora das condições acima ocorrer, o verde se acende, ou seja, o verde só acende se o valor ideal de luminosidade estiver definido.
